7475a16f72cf848e1b1eee7a5b0804b549aa52ba
Ahmad <achmadfaz@gmail.com>
2021-08-06 22:30:01 +0700

    jenis visa

---
+++ app/Models/JenisVisa.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class JenisVisa extends Model
+{
+    use HasFactory;
+    protected $table = 'jenis_visa';
+    protected $fillable = ['nama'];
+    public $timestamps = false;
+
+    public function setNamaAttribute($value)
+    {
+        return $this->attributes['nama'] = Str::ucfirst($value);
+    }
+}
