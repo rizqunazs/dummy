a2043fc6036ad71d75a5d2ad502cd73c9056b005
verysbm <verysetiabudi37@gmail.com>
2021-08-11 21:22:10 +0700

    edit head to E-Layanan

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,5 +1,6 @@
 <meta charset="utf-8" />
 <title>Sinotaris | @yield('title')</title>
+<title>E-Layanan | @yield('title')</title>
 <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta content="" name="description" />
